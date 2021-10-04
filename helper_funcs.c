0b3f07fc954628a16c7abfe2d370ed0415681be5
sequainoo <wazatoyung@gmail.com>
2021-10-04 22:34:43 +0000

    Add create_buffer to helper_funcs.c

--- helper_funcs.c
+++ helper_funcs.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * create_buffer - creates a buffer of a size
+ * @buffer_size: is the size in bytes for creating memory space
+ * Return: an address of the newly created space
+ */
+void* create_buffer(int buffer_size)
+{
+	char* buff;
+	buff = malloc(buffer_size);
+	if (buff == NULL)
+		return (NULL);
+	return (buff);
+}
