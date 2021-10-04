a34346ae26d9f5b516ef91333e1b268bf62f2217
sequainoo <wazatoyung@gmail.com>
2021-10-04 22:36:22 +0000

    Add get_format_func to helper_func.c

--- helper_funcs.c
+++ helper_funcs.c
@@ -13,3 +13,30 @@ void* create_buffer(int buffer_size)
 		return (NULL);
 	return (buff);
 }
+
+/**
+ * get_format_func - gets the function for a particular format specifier
+ * @format_char: the conversion character
+ * Return: a pointer to a function for that conversion character
+ */
+char* (*get_format_func(char format_char))(va_list)
+{
+	int i = 0;
+
+	format_object arr[] = {
+		{'c', handle_c},
+		{'s', handle_s},
+		{'d', handle_d},
+		{'i', handle_d},
+		{'\0', NULL}
+	};
+
+	while (arr[i].format_char != '\0')
+	{
+		if (arr[i].format_char == format_char)
+			return (arr[i].func);
+		i++;
+	}
+
+	return (NULL);
+}
