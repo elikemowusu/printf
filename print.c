491802ddd90e32ff9890d4fad30abe7247c43b5d
sequainoo <wazatoyung@gmail.com>
2021-10-04 22:51:25 +0000

    Add print.c with _printf

---
+++ print.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _printf - mini version of the standard printf
+ * @format: the string to be  used for printing
+ * Return: an interger representing the number of bytes written
+ */
+
+int _printf(const char *format, ...);
