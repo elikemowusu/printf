262ae121e2bbc9d7f1386c20e7b0928d4a04287a
sequainoo <wazatoyung@gmail.com>
2021-10-04 22:44:51 +0000

    Add write_buffer to helper_func.h

--- helper_funcs.c
+++ helper_funcs.c
@@ -56,3 +56,19 @@ int check_buffer(char* buffer, int buffer_pos)
 	}
 	return (buffer_pos);
 }
+
+/**
+ * write_buffer - prints buffer, then frees it and frees va_list
+ * @buffer: buffer holding print-ables
+ * @len: length of print-able string
+ * @list: va_list
+ */
+void write_buffer(char *buffer, int len, va_list list)
+{
+	char *buff;
+
+	buff = realloc(buffer, len); /* realloc to correct size */
+	write(1, buff, len); /* print */
+
+	free(buff); va_end(list);
+}
