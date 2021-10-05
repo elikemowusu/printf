73b12c64e36324eeb37717fbfcc9fba9c704aba5
sequainoo <wazatoyung@gmail.com>
2021-10-05 12:04:43 +0000

    Clean double declarations

--- helper_funcs.c
+++ helper_funcs.c
@@ -107,38 +107,3 @@ write(1, buff, len); /* print */
 
 free(buff); va_end(list);
 }
-
-/**
- * _strcpy - Copies information from each element
- * @dest: destination file
- * @src: source file
- * Return: array
- */
-char *_strcpy(char *dest, char *src)
-{
-int i = 0;
-
-while (src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-
-dest[i] = src[i];
-return (dest);
-}
-
-/**
- * _strlen - prints length string in integer
- * @s: string passed to function
- * Return: a
- */
-int _strlen(char *s)
-{
-int i = 0;
-
-while (*(s + i))
-i++;
-
-return (i);
-}
